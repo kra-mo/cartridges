fd395dfa365e6d6c8cd3989f335670317ec84b0b
kramo <93832451+kra-mo@users.noreply.github.com>
2023-03-16 01:09:56 +0100

    Cleanups

--- src/utils/get_cover.py
+++ src/utils/get_cover.py
@@ -25,6 +25,7 @@ from gi.repository import GdkPixbuf
 def get_cover(game_id, parent_widget):
     if game_id in parent_widget.pixbufs.keys():
         return parent_widget.pixbufs[game_id]
+
     cover_path = os.path.join(
         os.environ.get("XDG_DATA_HOME"), "cartridges", "covers", game_id + ".jpg"
     )
