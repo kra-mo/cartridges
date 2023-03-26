f5acf6d25664607b88b3128e82a6d17611bb29c3
K.B.Dharun Krishna <kbdharunkrishna@gmail.com>
2023-03-26 23:40:41 +0530

    revert: main.py release notes change

--- src/main.py
+++ src/main.py
@@ -38,10 +38,6 @@ from .save_games import save_games
 from .steam_parser import steam_parser
 from .window import CartridgesWindow
 
-release_notes = """
-<p>First stable release</p>
-"""
-
 class CartridgesApplication(Adw.Application):
     def __init__(self):
         super().__init__(
@@ -130,9 +126,7 @@ class CartridgesApplication(Adw.Application):
             issue_url="https://github.com/kra-mo/cartridges/issues/new",
             website="https://github.com/kra-mo/cartridges",
             # Translators: Replace this with your name for it to show up in the about window
-            translator_credits=_("translator_credits"),
-            release_notes_version=("1.1"),
-            release_notes=_(release_notes)
+            translator_credits=_("translator_credits")
         )
         about.present()
 
