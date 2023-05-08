34ab65c4d21db5cf12c90b3152e178045770908f
kramo <93832451+kra-mo@users.noreply.github.com>
2023-05-08 11:40:58 +0200

    Fix SGDB loading logic

--- src/utils/steamgriddb.py
+++ src/utils/steamgriddb.py
@@ -27,8 +27,7 @@ class SGDBSave:
             Gio.Task.new(None, None, self.task_done).run_in_thread(create_func(game))
 
     def update_cover(self, task, game):
-        if not self.importer:
-            game.set_loading(1)
+        game.set_loading(1)
 
         if (
             not (
