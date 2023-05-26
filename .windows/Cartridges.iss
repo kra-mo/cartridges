11cd2129b848990d6ef0cf8678fac35e88293910
kramo <93832451+kra-mo@users.noreply.github.com>
2023-05-26 20:24:39 +0200

    v1.5.1

--- .windows/Cartridges.iss
+++ .windows/Cartridges.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Cartridges"
-#define MyAppVersion "1.5"
+#define MyAppVersion "1.5.1"
 #define MyAppPublisher "kramo"
 #define MyAppURL "https://github.com/kra-mo/cartridges"
 #define MyAppExeName "pythonw.exe"
--- src/main.py
+++ src/main.py
@@ -109,7 +109,7 @@ class CartridgesApplication(Adw.Application):
             application_name=_("Cartridges"),
             application_icon="hu.kramo.Cartridges",
             developer_name="kramo",
-            version="1.5",
+            version="1.5.1",
             developers=[
                 "kramo https://kramo.hu",
                 "Arcitec https://github.com/Arcitec",
