9d7a6d8ea44e5ae1f9b52fda943832136f19f858
kramo <93832451+kra-mo@users.noreply.github.com>
2023-06-17 16:27:10 +0200

    Add copyright headers

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,5 +1,24 @@
+# decorators.py
+#
+# Copyright 2023 Geoffrey Coulaud
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from pathlib import Path
-from os import PathLike, environ
+from os import PathLike
 from functools import wraps
 
 from src import shared  # pylint: disable=no-name-in-module
