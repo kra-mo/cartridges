1b2d801354688d51838644064f2229ac63e02f45
Jamie Gravendeel <me@jamie.garden>
2026-02-13 10:10:45 +0100

    sources: Remove n-items workaround

--- cartridges/ui/sources.py
+++ cartridges/ui/sources.py
@@ -22,14 +22,6 @@ class SourceSidebarItem(Adw.SidebarItem):  # pyright: ignore[reportAttributeAcce
     def __init__(self, source: Source | None = None, **kwargs: Any):
         super().__init__(**kwargs)
 
-        # https://gitlab.gnome.org/GNOME/gtk/-/issues/7959
-        self._model_signals = GObject.SignalGroup(target_type=Gio.ListModel)
-        self._model_signals.connect_closure(
-            "items-changed",
-            lambda model, *_: model.notify("n-items"),
-            after=False,
-        )
-
         flags = GObject.BindingFlags.DEFAULT
         self._source_bindings = GObject.BindingGroup()
         self._source_bindings.bind("name", self, "title", flags)
@@ -38,10 +30,7 @@ class SourceSidebarItem(Adw.SidebarItem):  # pyright: ignore[reportAttributeAcce
 
         self.model = Gtk.FilterListModel(filter=games.filter_, watch_items=True)  # pyright: ignore[reportCallIssue]
         self.model.bind_property(
-            "n-items",
-            self,
-            "visible",
-            GObject.BindingFlags.SYNC_CREATE,
+            "n-items", self, "visible", GObject.BindingFlags.SYNC_CREATE
         )
         self.bind_property("source", self.model, "model")
         self.source = source
